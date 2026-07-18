6666d5a5cb1279b96c4c592c4dc12f71a2f71ecf
cochiay <cochiay@icloud.com>
2026-07-18 16:19:09 +0800

    完成表單check, 需再檢查導覽觸碰顏色功能

--- midtest/main.js
+++ midtest/main.js
@@ -1,18 +1,23 @@
+const form = document.getElementById("onlyForm");
+const userMail = document.getElementById("exampleFormControlInput1");
+const res = document.getElementById("exampleFormControlTextarea1");
 
-
-
-const form = document.getElementById('onlyForm')
-// const userMail = document.getElementById('exampleFormControlInput1')
-// const res = document.getElementById('exampleFormControlTextarea1')
-
-
-document.getElementById("resSub").addEventListener('click', checkSubmit);
+document.getElementById("resSub").addEventListener("click", checkSubmit);
 
 function checkSubmit(event) {
-    event.preventDefault();
-    alert('已送出信息');
-    console.log('送出成功');
-    setTimeout(function () { location.reload(); }, 2000);
+  //   event.preventDefault();
 
+  let a = userMail.value.trim();
+  let b = res.value.trim();
+
+  if ((a === "") | (b === "")) {
+    alert("內容不可為空白！");
+    return;
+  }
+
+  alert("已送出信息！");
+  console.log("送出成功");
+  setTimeout(function () {
+    location.reload();
+  }, 2000);
 }
-
